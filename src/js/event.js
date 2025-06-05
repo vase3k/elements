eb04f92fdb297dfb77a9c5b79f8f0f116d755d69
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-05 13:16:56 +0300

    forEach remove event

--- src/js/event.js
+++ src/js/event.js
@@ -7,24 +7,36 @@ const btn = document.querySelectorAll('button'),
 //     console.log(e.target);
 // };
 
-let i = 0;
+// let i = 0;
 
-const deleteElement = (e) => {
-    console.log(e.target, e.currentTarget, e.type);
-    // i++;
-    // if (i == 1) {
-    //     e.currentTarget.removeEventListener('click', deleteElement);
-    // }
-};
+// const deleteElement = (e) => {
+//     console.log(e.target, e.currentTarget, e.type);
+//     // i++;
+//     // if (i == 1) {
+//     //     e.currentTarget.removeEventListener('click', deleteElement);
+//     // }
+// };
 
-btn.forEach((e) => {
-    e.addEventListener('click', deleteElement, { once: true });
-});
+// btn.forEach((e) => {
+//     e.addEventListener('click', deleteElement, { once: true });
+// });
 
 // btn.addEventListener('click', deleteElement);
 // overlay.addEventListener('click', deleteElement);
 
-link.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log(e.target);
-});
+// link.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     console.log(e.target);
+// });
+
+((btn) => {
+    let i = 0;
+    btn.forEach((btn) => {
+        const deleteElement = (e) => {
+            console.log(e.type, e.target);
+            i++;
+            i > 0 && e.target.removeEventListener('click', deleteElement);
+        };
+        btn.addEventListener('click', deleteElement);
+    });
+})(document.querySelectorAll('button'));
