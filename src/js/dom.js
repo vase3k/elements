006a629db30201f36f76398c9178f6120cf37fb7
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-05 21:32:23 +0300

    DOM elements

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log(document.body);
+//console.log(document.body);
+//console.log(document.documentElement);
+//console.log(document.body.childNodes);
+//console.log(document.body.firstElementChild);
+//console.log(document.body.lastElementChild);
+//console.log(document.querySelector('#current').parentNode.parentNode);
+//console.log(document.querySelector('[data-current="3"]').parentElement);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName === '#text') continue;
+    console.log(node);
+}
