38531166c2f675dd80b6621c7d153316da4a0d31
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-07 17:20:17 +0300

    mobile events touch

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import '/src/sass/style.scss';
-import '/src/js/dataBase.js';
+//import '/src/js/dataBase.js';
 //import '/src/js/event.js';
 //import '/src/js/dom.js';
 //import '/src/js/recursion.js';
+//import '/src/js/mobile.js';
--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import createFoldersPlugin from './src/js/createFolder';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    //base: '/elements/dist/',
+    base: '/elements/dist/',
     build: {
         rollupOptions: {
             input: {
                 index: resolve(__dirname, 'index.html'),
                 event: resolve(__dirname, 'event.html'),
                 dom: resolve(__dirname, 'dom.html'),
+                mobile: resolve(__dirname, 'mobile.html'),
             },
         },
     },
