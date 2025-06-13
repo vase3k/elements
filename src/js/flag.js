1897b797285f59b04d382e40583f17f232d5c69d
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-13 20:19:52 +0300

    iterator

--- src/js/flag.js
+++ src/js/flag.js
@@ -9,6 +9,23 @@ const user = {
     },
 };
 
+for (const key of Object.keys(user)) {
+    //  console.log(user[key]);
+}
+
+const arr = [1, 2, 3, 4];
+
+Array.prototype.something = function () {};
+
+for (const key of arr) {
+    // console.log(key);
+}
+
+// const str = 'gggvob';
+
+// for (const key of str) {
+//     console.log(key);
+// }
 //Object.defineProperties(user, { name: { writable: false } });
 
 // Object.defineProperties(user, {
@@ -22,10 +39,46 @@ const user = {
 //Object.freeze(user);
 //console.log(Object.getOwnPropertyDescriptor(user, 'name'));
 
-console.log(Object.getOwnPropertyDescriptor(user, Object.getOwnPropertySymbols(user)[0]));
-Object.defineProperty(user, Object.getOwnPropertySymbols(user)[0], { value: 20 });
-console.log(Object.getOwnPropertyDescriptor(user, Object.getOwnPropertySymbols(user)[0]));
+// console.log(Object.getOwnPropertyDescriptor(user, Object.getOwnPropertySymbols(user)[0]));
+// Object.defineProperty(user, Object.getOwnPropertySymbols(user)[0], { value: 20 });
+// console.log(Object.getOwnPropertyDescriptor(user, Object.getOwnPropertySymbols(user)[0]));
 
-const keys = Object.keys(user),
-    values = Object.values(user),
-    entries = Object.entries(user);
+// const keys = Object.keys(user),
+//     values = Object.values(user),
+//     entries = Object.entries(user);
+
+const salaries = {
+    john: 500,
+    ivan: 1000,
+    ann: 5000,
+    SayHello: function () {
+        console.log(this.john + 'salary is');
+    },
+};
+
+salaries[Symbol.iterator] = function () {
+    return {
+        currency: this.john,
+        last: this.ann,
+
+        next() {
+            if (this.currency < this.last) {
+                this.currency = this.currency + 500;
+                return { done: false, value: this.currency };
+            } else {
+                return { done: true };
+            }
+        },
+    };
+};
+
+const iterator = salaries[Symbol.iterator]();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+// for (let res of salaries) {
+//     console.log(res);
+// }
